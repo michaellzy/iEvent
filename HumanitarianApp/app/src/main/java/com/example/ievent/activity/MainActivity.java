0a9e5df2318db355d4910178f397523dee207b12
michaellzy <u7690985@anu.edu.au>
2024-04-11 22:41:35 +1000

    init app

---
+++ HumanitarianApp/app/src/main/java/com/example/ievent/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ievent.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ievent.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
