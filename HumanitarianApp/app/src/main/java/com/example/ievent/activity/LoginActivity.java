bf4774b534e49e4e5dd841310eb9e99afde29b71
michaellzy <u7690985@anu.edu.au>
2024-04-12 23:14:55 +1000

    add progress bar, add remember password

--- HumanitarianApp/app/src/main/java/com/example/ievent/activity/LoginActivity.java
+++ HumanitarianApp/app/src/main/java/com/example/ievent/activity/LoginActivity.java
@@ -3,19 +3,33 @@ package com.example.ievent.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.ievent.R;
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class LoginActivity extends AppCompatActivity {
     private EditText emailEditText;
     private EditText passwordEditText;
     private FirebaseAuth mAuth;
+
+    private ProgressBar progressBar;
+
+    private CheckBox checkBox;
+
+
+    private static final String PREF = "LOGIN_PREF";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +39,11 @@ public class LoginActivity extends AppCompatActivity {
 
         emailEditText = findViewById(R.id.editText_login_EmailAddress);
         passwordEditText = findViewById(R.id.editText_login_pwd);
+        progressBar = findViewById(R.id.progress_login);
+        checkBox = findViewById(R.id.checkBox_remember);
         Button loginButton = findViewById(R.id.button_login);
         Button signUpButton = findViewById(R.id.button_signup);
+
 
         loginButton.setOnClickListener(v -> loginUser());
 
@@ -35,27 +52,56 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
             startActivity(intent);
         });
+        getPrefData();
     }
 
     private void loginUser() {
         String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
-
+        progressBar.setVisibility(View.VISIBLE);
+        SharedPreferences sp = getSharedPreferences(PREF, MODE_PRIVATE);
+        if (checkBox.isChecked()) {
+            boolean isChecked = checkBox.isChecked();
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("email", email);
+            editor.putString("pwd", password);
+            editor.putBoolean("checked", isChecked);
+            editor.apply();
+        } else {
+            sp.edit().clear().apply();
+        }
         if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
-            mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, task -> {
-                        if (task.isSuccessful()) {
-                            // login succeed, jump to main
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(intent);
-                            finish();
-                        } else {
-                            // login failed, show failed message
-                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    // login succeed, jump to main
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // login failed, show failed message
+                    Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            });
         } else {
             Toast.makeText(this, "Email and password are required.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void getPrefData() {
+        SharedPreferences sp = getSharedPreferences(PREF, MODE_PRIVATE);
+        if (sp.contains("email")) {
+            String storedEmail = sp.getString("email", "email not found");
+            emailEditText.setText(storedEmail);
+        }
+        if (sp.contains("pwd")) {
+            String storedPwd = sp.getString("pwd", "pwd not found");
+            passwordEditText.setText(storedPwd);
+        }
+        if (sp.contains("checked")) {
+            boolean storedIsChecked = sp.getBoolean("checked", false);
+            checkBox.setChecked(storedIsChecked);
         }
     }
 }
--- HumanitarianApp/app/src/main/java/com/example/ievent/activity/SignupActivity.java
+++ HumanitarianApp/app/src/main/java/com/example/ievent/activity/SignupActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.ievent.R;
@@ -16,6 +18,8 @@ public class SignupActivity  extends AppCompatActivity{
     private EditText passwordEditText;
     private EditText confirmEditText;
     private FirebaseAuth mAuth;
+
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,7 @@ public class SignupActivity  extends AppCompatActivity{
         emailEditText = findViewById(R.id.editText_signup_EmailAddress);
         passwordEditText = findViewById(R.id.editText_signup_pwd);
         confirmEditText = findViewById(R.id.editText_signup_confirm);
+        progressBar = findViewById(R.id.progress_register);
         Button loginButton = findViewById(R.id.button_signup_login);
         Button signUpButton = findViewById(R.id.button_signup_signup);
 
@@ -41,7 +46,7 @@ public class SignupActivity  extends AppCompatActivity{
         String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
         String confirmPassword = confirmEditText.getText().toString().trim();
-
+        progressBar.setVisibility(View.VISIBLE);
         if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(confirmPassword)) {
             if (!password.equals(confirmPassword)) {
                 // if pwd != confirm, show the fail message
@@ -50,9 +55,11 @@ public class SignupActivity  extends AppCompatActivity{
                 // pwd correct
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(this, task -> {
+                            progressBar.setVisibility(View.GONE);
                             if (task.isSuccessful()) {
                                 Toast.makeText(SignupActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                // Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
                                 startActivity(intent);
                                 finish();
                             } else {
