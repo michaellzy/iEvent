d64673e2d5c63728efaba4ff7f2faecfd3e1c22c
Tengkai Wang <734394415@qq.com>
2024-04-16 22:24:40 +1000

    add event operation, get event by type operation, refine the method to use callback interface.

--- HumanitarianApp/app/src/main/java/com/example/ievent/activity/BaseActivity.java
+++ HumanitarianApp/app/src/main/java/com/example/ievent/activity/BaseActivity.java
@@ -6,31 +6,33 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ievent.database.data_manager.EventDataManager;
-import com.example.ievent.database.listener.UserDataListener;
+import com.example.ievent.database.listener.EventDataListener;
 import com.example.ievent.entity.Event;
-import com.example.ievent.entity.User;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class BaseActivity extends AppCompatActivity implements UserDataListener {
+import java.util.ArrayList;
+
+public class BaseActivity extends AppCompatActivity {
     protected FirebaseAuth mAuth;
 
-    protected Firebase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this, "BaseActivity", Toast.LENGTH_SHORT).show();
 
-        for (Event bollywoodEventsSydney : EventDataManager.getInstance().getAllEventsByType("bollywood events sydney")) {
-            System.out.println(bollywoodEventsSydney);
-        }
+
+        EventDataManager.getInstance().getAllEventsByType("boat party", new EventDataListener() {
+            @Override
+            public void onSuccess(ArrayList<Event> data) {
+                Toast.makeText(BaseActivity.this, data.size() + "", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(String errorMessage) {
+
+            }
+        });
     }
 
-    @Override
-    public void onCurrentUser(User user) {
-    }
-
-    @Override
-    public void onFailure(String errorMessage) {
-    }
 }
