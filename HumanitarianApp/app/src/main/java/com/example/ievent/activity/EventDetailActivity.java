f225618520c0874bfea5f1ef15ccb75887afe5b3
Tengkai Wang <u7733280@anu.edu.au>
2024-04-20 22:04:57 +1000

    google map 2

--- HumanitarianApp/app/src/main/java/com/example/ievent/activity/EventDetailActivity.java
+++ HumanitarianApp/app/src/main/java/com/example/ievent/activity/EventDetailActivity.java
@@ -12,5 +12,19 @@ public class EventDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
+
+        setVariables();
     }
+
+    private void setVariables() {
+        findViewById(R.id.imageView_map).setOnClickListener(v -> {
+            // Open map activity and add description to the intent
+            android.content.Intent intent = new android.content.Intent(this, MapActivity.class);
+            // here put the name of destination
+            intent.putExtra("destination", "ANU");
+            startActivity(intent);
+        });
+    }
+
+
 }
--- HumanitarianApp/app/src/main/java/com/example/ievent/activity/MapActivity.java
+++ HumanitarianApp/app/src/main/java/com/example/ievent/activity/MapActivity.java
@@ -13,14 +13,22 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-// Implement OnMapReadyCallback.
+/**
+ * This class is used to display the map.
+ */
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    ActivityMapBinding mapActivityBinding;
+
+    String destination;
+
+    LatLng destinationLatLng;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityMapBinding mapActivityBinding = ActivityMapBinding.inflate(getLayoutInflater());
+        mapActivityBinding = ActivityMapBinding.inflate(getLayoutInflater());
 
         // Set the layout file as the content view.
         setContentView(mapActivityBinding.getRoot());
@@ -31,9 +39,32 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
+
+        setVariables();
     }
 
-    // Get a handle to the GoogleMap object and display marker.
+    /**
+     * This method is used to set the variables for the activity.
+     */
+    private void setVariables() {
+        // get the destination from the intent
+        destination = getIntent().getStringExtra("destination");
+        // get the latitude and longitude of the destination by searching it from the google map api
+        // and set the destinationLatLng
+
+        mapActivityBinding.btnReturn.setOnClickListener(v -> finish());
+        mapActivityBinding.btnOpenGoogleMap.setOnClickListener(v -> {
+            // Open Google Map
+            String uri = "geo:0,0?q=-35.282001,149.128998(ANU)";
+            startActivity(new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse(uri)));
+        });
+    }
+
+
+    /**Get a handle to the GoogleMap object and display marker.
+     *
+     * @param googleMap
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // set the position in Canberra ANU and set the center as it
@@ -42,6 +73,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         LatLng target = new LatLng(-35.282001, 149.128998);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(target));
+
+        // Add a large marker at ANU.
         googleMap.addMarker(new MarkerOptions()
                 .position(target)
                 .title("ANU"));
