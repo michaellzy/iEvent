60f9a017d68b475eb2941c5db97b92fbfc14633a
Tengkai Wang <u7733280@anu.edu.au>
2024-04-22 10:50:13 +1000

    add places api

--- HumanitarianApp/app/src/main/java/com/example/ievent/activity/MapActivity.java
+++ HumanitarianApp/app/src/main/java/com/example/ievent/activity/MapActivity.java
@@ -2,7 +2,11 @@ package com.example.ievent.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.example.ievent.databinding.ActivityMapBinding;
 import com.google.android.gms.maps.CameraUpdate;
@@ -12,6 +16,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.api.net.SearchByTextRequest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.grpc.android.BuildConfig;
 
 /**
  * This class is used to display the map.
@@ -22,6 +36,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     String destination;
 
     LatLng destinationLatLng;
+
+    PlacesClient placesClient;
 
 
     @Override
@@ -40,17 +56,23 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
-        setVariables();
+
+        try {
+            setVariables();
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
      * This method is used to set the variables for the activity.
      */
-    private void setVariables() {
+    private void setVariables() throws PackageManager.NameNotFoundException {
         // get the destination from the intent
         destination = getIntent().getStringExtra("destination");
-        // get the latitude and longitude of the destination by searching it from the google map api
-        // and set the destinationLatLng
+
+        setPlacesAPI();
+
 
         mapActivityBinding.btnReturn.setOnClickListener(v -> finish());
         mapActivityBinding.btnOpenGoogleMap.setOnClickListener(v -> {
@@ -58,6 +80,30 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             String uri = "geo:0,0?q=-35.282001,149.128998(ANU)";
             startActivity(new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse(uri)));
         });
+    }
+
+    /**
+     * This method is used to set the Places API and get the PlacesClient.
+     */
+    private void setPlacesAPI() throws PackageManager.NameNotFoundException {
+        // Define a variable to hold the Places API key.
+        ApplicationInfo applicationInfo = this.getPackageManager().getApplicationInfo(
+                this.getPackageName(), PackageManager.GET_META_DATA
+        );
+        Bundle metaData = applicationInfo.metaData;
+
+        String apiKey = metaData.getString("com.google.android.geo.API_KEY");
+
+        // Log an error if apiKey is not set.
+        if (TextUtils.isEmpty(apiKey) || apiKey.equals("DEFAULT_API_KEY")) {
+            Log.e("Places test", "No api key");
+            finish();
+            return;
+        }
+        // Initialize the SDK
+        Places.initializeWithNewPlacesApiEnabled(getApplicationContext(), apiKey);
+        // Create a new PlacesClient instance
+        placesClient = Places.createClient(this);
     }
 
 
@@ -74,9 +120,34 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         LatLng target = new LatLng(-35.282001, 149.128998);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(target));
 
-        // Add a large marker at ANU.
+        // Add a large marker at the destination in the map
         googleMap.addMarker(new MarkerOptions()
                 .position(target)
-                .title("ANU"));
+                .title(destination));
+
+        searchDestination(destination);
+    }
+
+    public void searchDestination(String destination) {
+        // search the destination from the google map api and set the destinationLatLng
+        // Specify the list of fields to return.
+        final List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+
+        // Define latitude and longitude coordinates of the search area.
+        LatLng southWest = new LatLng(37.38816277477739, -122.08813770258874);
+        LatLng northEast = new LatLng(37.39580487866437, -122.07702325966572);
+
+        // Use the builder to create a SearchByTextRequest object.
+        final SearchByTextRequest searchByTextRequest = SearchByTextRequest.builder("Spicy Vegetarian Food", placeFields)
+                .setMaxResultCount(10)
+                .setLocationRestriction(RectangularBounds.newInstance(southWest, northEast)).build();
+
+        // Call PlacesClient.searchByText() to perform the search.
+        // Define a response handler to process the returned List of Place objects.
+        placesClient.searchByText(searchByTextRequest)
+                .addOnSuccessListener(response -> {
+                    List<Place> places = response.getPlaces();
+                });
+
     }
 }
