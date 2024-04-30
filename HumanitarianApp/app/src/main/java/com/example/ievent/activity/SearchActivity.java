3c3cbde71adb7723531dba7c76d19dc0d4cad609
Li Haolin <u7772332@anu.edu.au>
2024-04-30 20:28:44 +1000

    Implement search by query function

--- HumanitarianApp/app/src/main/java/com/example/ievent/activity/SearchActivity.java
+++ HumanitarianApp/app/src/main/java/com/example/ievent/activity/SearchActivity.java
@@ -1,6 +1,8 @@
 package com.example.ievent.activity;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.media.RouteListingPreference;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -10,8 +12,11 @@ import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ievent.R;
+import com.example.ievent.adapter.RecommendedActivitiesAdapter;
 import com.example.ievent.database.listener.EventDataListener;
 import com.example.ievent.entity.Event;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -19,13 +24,44 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends BaseActivity {
+
+    private RecyclerView recyclerView;
+    private SearchView searchView;
+    private RecommendedActivitiesAdapter recommendedActivitiesAdapter;
+    private List<Event> eventList = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         Button filterbutton = findViewById(R.id.filter_button);
+
+
+        recyclerView = findViewById(R.id.searchResultRecyclerView);
+        recommendedActivitiesAdapter = new RecommendedActivitiesAdapter(eventList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recommendedActivitiesAdapter);
+
+        searchView = findViewById(R.id.search_view);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                performSearch(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+//                performSearch(newText);
+                return false;
+            }
+        });
 
         filterbutton.setOnClickListener(v -> {
             BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(SearchActivity.this);
@@ -59,8 +95,8 @@ public class SearchActivity extends BaseActivity {
         });
 
 
-        initSearchWidgets();
-        setUpData();
+//        initSearchWidgets();
+//        setUpData();
     }
 
     private void initSearchWidgets(){
@@ -101,4 +137,24 @@ public class SearchActivity extends BaseActivity {
             }
         });
     }
+    private void performSearch(String query) {
+        Log.d("SearchActivity", "performSearch: Start, Query: " + query);
+        db.getAllEventsByFuzzyName(query, new EventDataListener() {
+            @Override
+            public void onSuccess(ArrayList<Event> events) {
+                Log.d("SearchActivity", "Search onSuccess: Number of events found: " + events.size());
+                eventList.clear();
+                eventList.addAll(events);
+                recommendedActivitiesAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(String error) {
+                Log.e("SearchActivity", "Search onFailure: " + error);
+                Toast.makeText(SearchActivity.this, "Search Error: " + error, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }
