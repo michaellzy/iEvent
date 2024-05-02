ef8f41177e305aa75f21935ebdc284ef5584dd7c
Qianwen Shen <u7726387@anu.edu.au>
2024-05-02 22:44:01 +1000

    ticket

--- HumanitarianApp/app/src/main/java/com/example/ievent/activity/UserAcitivity.java
+++ HumanitarianApp/app/src/main/java/com/example/ievent/activity/UserAcitivity.java
@@ -123,6 +123,7 @@ public class UserAcitivity extends BaseActivity {
     }
 
 
+
     private ActivityResultLauncher getCropImageActivityResultLauncher() {
         return registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
