67cdd036b9ff2fcb61d4f2a93bb2ac38053d6583
Tengkai Wang <734394415@qq.com>
2024-05-08 13:49:49 +1000

    wifip2pmanager connected

--- HumanitarianApp/app/src/main/java/com/example/ievent/global/WiFiDirectBroadcastReceiver.java
+++ HumanitarianApp/app/src/main/java/com/example/ievent/global/WiFiDirectBroadcastReceiver.java
@@ -5,11 +5,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
-import android.util.Log;
+import android.os.Build;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 
 import com.example.ievent.activity.P2PchatActivity2;
@@ -23,6 +25,7 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
     private WifiP2pManager.Channel channel;
     private P2PchatActivity2 activity;
 
+
     public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
                                        P2PchatActivity2 activity) {
         super();
@@ -31,6 +34,7 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
         this.activity = activity;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
@@ -45,8 +49,8 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
                 Toast.makeText(context, "Wifi P2P is not enabled", Toast.LENGTH_SHORT).show();
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
             // Call WifiP2pManager.requestPeers() to get a list of current peers
-            Toast.makeText(context, "Peers changed", Toast.LENGTH_SHORT).show();
             if (manager != null) {
                 if (ActivityCompat.checkSelfPermission(
                         context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(context, Manifest.permission.NEARBY_WIFI_DEVICES) != PackageManager.PERMISSION_GRANTED) {
@@ -55,10 +59,33 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
                     ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.NEARBY_WIFI_DEVICES}, 1);
                     return;
                 }
-                manager.requestPeers(channel, peers -> Toast.makeText(context, "Peers available, you find" + peers.getDeviceList().size() + "devices", Toast.LENGTH_SHORT).show());
+                manager.requestPeers(channel, peers -> {
+                    //obtain a peer from the WifiP2pDeviceList
+                    WifiP2pDevice device = peers.getDeviceList().iterator().next();
+                    WifiP2pConfig config = new WifiP2pConfig();
+                    config.deviceAddress = device.deviceAddress;
+
+
+                    // Connect to the first device found
+                        manager.connect(channel, config, new WifiP2pManager.ActionListener() {
+
+                            @Override
+                            public void onSuccess() {
+                                //success logic
+//                                Toast.makeText(context, "Connected to " + device.deviceName, Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onFailure(int reason) {
+                                Toast.makeText(context, "Connection failed: " + reason, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                });
+
             }
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
             // Respond to new connection or disconnections
+            Toast.makeText(context, "Connection changed", Toast.LENGTH_SHORT).show();
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
             // Respond to this device's wifi state changing
         }
